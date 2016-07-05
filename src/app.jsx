4c7507d9f209fa66f768d1763a344f2a3b19ce62
ymcatar <ymcatar@gmail.com>
2016-07-05 16:48:50 +0800

    refactor

--- src/app.jsx
+++ src/app.jsx
@@ -1,20 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import DemoOne from './demo-one.jsx';
-import DemoTwo from './demo-two.jsx';
-import DemoThree from './demo-three.jsx';
+import DemoOne from './DemoOne.jsx';
+import DemoTwo from './DemoTwo.jsx';
+import DemoThree from './DemoThree.jsx';
 
 const styles = {
   title: {
     width: '100%',
     textAlign: 'center',
     marginTop: 30,
-    fontWeight: 200
-  },
-  subtitle: {
-    width: '100%',
-    textAlign: 'center',
+    marginBottom: 40,
     fontWeight: 200
   },
   container: {
@@ -28,7 +24,6 @@ export default class App extends React.Component {
     return (
       <div>
         <h1 style={styles.title}>React Animation Demo</h1>
-        <h5 style={styles.subtitle}>interruptable animation that only depends on the state</h5>
         <div style={styles.container}>
           <DemoOne />
           <DemoTwo />
